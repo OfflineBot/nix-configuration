645056fcee42b47fc93b31b4ca089e89768d62d1
OfflineBot <FlintStonelg@gmail.com>
2026-06-10 15:53:02 +0200

    first commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+    description = "NixOS configuration";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    outputs = { nixpkgs, ... }:
+        {
+            nixosConfigurations."nixos" = nixpkgs.lib.nixosSystem {
+                system = "x86_64-linux";
+                modules = [ ./configuration.nix ];
+            };
+        };
+}
