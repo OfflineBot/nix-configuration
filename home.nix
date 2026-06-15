6973b0fef30dd75f4a95570d7819204d41f6e162
OfflineBot <FlintStonelg@gmail.com>
2026-06-15 13:26:26 +0200

    added xournal

--- home.nix
+++ home.nix
@@ -18,6 +18,7 @@
         gcc 
         quickshell
         wl-clipboard
+        sshfs
         btop
         cowsay
         go rustup
@@ -27,6 +28,7 @@
         pavucontrol
         brightnessctl
         hyprlock
+        xournalpp
 
         unzip zip
         claude-code
